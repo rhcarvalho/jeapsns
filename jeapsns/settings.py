9df2e71918d4bd9bb8eba389f28c8fec029a54b5
Rodolfo Carvalho <rhcarvalho@gmail.com>
2013-03-09 07:43:52 +0800

    Fix path to templates.
    Set the path to be relative to the project files, independent of the system where it is running.

--- jeapsns/settings.py
+++ jeapsns/settings.py
@@ -1,4 +1,5 @@
 # Django settings for jeapsns project.
+import os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -108,7 +109,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/peter/jeapsns/templates",
+    os.path.join(os.path.dirname(__file__), os.path.pardir, 'templates').replace('\\','/'),
 )
 
 INSTALLED_APPS = (
